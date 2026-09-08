2d5ab3ffc5b9a54a138d97ebe25c3a066ab8ad7f
Enzo <90078316+EnzoSuperCraftZ@users.noreply.github.com>
2026-09-08 17:57:31 -0400

    insert into cliente

---
+++ scripts/insert_into_cliente.sql
@@ -0,0 +1,8 @@
+NSERT INTO cliente (nome, cpf) VALUES
+('Ana Souza', '12345678901'),
+('Carlos Pereira', '98765432100'),
+('Mariana Lima', '45678912345'),
+('Pedro Henrique', '67420133796'),
+('Charles Joaquim', '78239290231'),
+('João Victor', '03502193128'),
+('Lucas Storer', '28753289552'); 
