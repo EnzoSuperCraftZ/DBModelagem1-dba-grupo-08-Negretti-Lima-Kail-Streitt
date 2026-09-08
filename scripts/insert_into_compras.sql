e68421f9788ea304f081c5dd017d0190028c72f4
Enzo <90078316+EnzoSuperCraftZ@users.noreply.github.com>
2026-09-08 17:53:18 -0400

    insert into compras

---
+++ scripts/insert_into_compras.sql
@@ -0,0 +1,4 @@
+INSERT INTO compra (cliente_id, sessao_caixa_id, valor, forma, metodo_pagamento) VALUES
+(36, 13, 14.50, 'concluida', 'cartao_debito'),
+(37, 14, 12.00, 'concluida', 'pix'),
+(38, 15, 6.50, 'concluida', 'cartao_credito');
