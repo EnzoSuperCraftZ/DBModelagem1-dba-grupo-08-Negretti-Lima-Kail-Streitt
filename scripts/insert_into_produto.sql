8d31048fdffa611c5bedc53fa0eb0713e08f45a3
Enzo <90078316+EnzoSuperCraftZ@users.noreply.github.com>
2026-09-08 17:58:46 -0400

    insert into produto

---
+++ scripts/insert_into_produto.sql
@@ -0,0 +1,9 @@
+INSERT INTO produto (nome, preco) VALUES
+('Café Expresso', 6.50),
+('Pão de Queijo', 4.00),
+('Suco de Laranja 300ml', 8.00),
+('Bolo de Cenoura (Fatia)', 7.50),
+('Coxinha', 8.00),
+('Morango do Amor', 15.00),
+('Fatia de Pizza (Qualquer sabor)', 8.00),
+('Nescau', 7.98); 
