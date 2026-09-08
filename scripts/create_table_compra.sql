f403c21bb9269b19207fd67066462ba8b9dd351c
Enzo <90078316+EnzoSuperCraftZ@users.noreply.github.com>
2026-09-08 17:51:03 -0400

    apenas create table em compra
    Removed the addition of sessao_caixa_id column and foreign key constraint from compra table.

--- scripts/create_table_compra.sql
+++ scripts/create_table_compra.sql
@@ -12,7 +12,4 @@ CREATE TABLE IF NOT EXISTS compra (
 
 
 
-ALTER TABLE compra 
-ADD COLUMN sessao_caixa_id INT NOT NULL,
-ADD CONSTRAINT fk_compra_sessao 
-    FOREIGN KEY (sessao_caixa_id) REFERENCES sessao_caixa(id);
+
