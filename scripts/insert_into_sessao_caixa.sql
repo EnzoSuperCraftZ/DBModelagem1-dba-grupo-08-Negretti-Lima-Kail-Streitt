3a670242ce846c208d298dd77d6978f31e6a0e09
Enzo <90078316+EnzoSuperCraftZ@users.noreply.github.com>
2026-09-08 18:04:30 -0400

    insert into sessao_caixa

---
+++ scripts/insert_into_sessao_caixa.sql
@@ -0,0 +1,7 @@
+INSERT INTO sessao_caixa(terminal, operador_nome, turno, saldo_inicial, status) VALUES
+("Caixa 01",	"Lucas Santos",	"manhã",	100.00,	"fechado"),
+("Caixa 01",	"Beatriz Rocha",	"tarde",	150.00,	"aberto"),
+("Caixa 02",	"Lucas Santos", "tarde",	300.00,	"aberto"),
+("Caixa 03",	"Leon Tamarez",	"manhã",	200.00,	"fechado"),
+("Caixa 03",	"Beatriz Rocha",	"tarde",	310.00,	"aberto"),
+("Caixa 02",	"Leon Tamarez",	"manhã",	401.00, "fechado");
